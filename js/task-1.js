b89b739e9cd2442f5f4fd112dbe583a36980e09a
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-03-27 19:18:51 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,14 @@
-const b = '200';
-console.log(b * b);
+function slugify(title) {
+    let toLowerCaseTitle = title.toLowerCase();
+    let slug = toLowerCaseTitle.split(' ').join('-')
+    return slug
+}
+
+
+
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
