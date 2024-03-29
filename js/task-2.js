335fbe1c98df0f09d5ef257aba22d43e1b12819b
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-03-29 15:13:35 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,11 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    let newArray = firstArray.concat(secondArray)
-    for (i = 0; i < maxLength; i++) {
-newArray.push(i)
-    }
-    return newArray
+    const newArray = firstArray.concat(secondArray)
+    if (newArray.length > maxLength) {
+        return newArray.slice(0, maxLength);
+    } else {
+        return newArray;
+   }
 }
-
 
 
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
@@ -14,3 +14,14 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+
+
+
+
+
+
+
+
+
+
